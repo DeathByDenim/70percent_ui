0d7735c9924e4b31fa9aed603344718642e36fe4
DeathByDenim <jarno@jarno.ca>
2015-03-24 08:24:18 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+70% UI
+======
+
+Mod for Planetary Annihilation to scale the UI by 70%.
---
+++ ui/mods/com.pa.DeathByDenim.70percent_ui/live_game.js
@@ -0,0 +1,11 @@
+(function() {
+/*
+	var oldheight = parseInt($('#build_bar').css('height'));
+	$('#build_bar').css('height', .7*oldheight + 'px');
+	
+	console.log("Old height was", oldheight);
+	
+	oldheight = parseInt($('#action_bar').css('width'));
+	$('#action_bar').css('width', .7*oldheight + 'px');
+*/
+})();
